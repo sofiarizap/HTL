1607b20cc71f1818a9ce7d2396401782bb20006d
sofiarizap <sofiaariza15@hotmail.com>
2024-11-18 10:20:21 -0500

    vue

--- e-commerce/src/main/java/SofiaAriza/e_commerce/Controladores/ProductoController.java
+++ e-commerce/src/main/java/SofiaAriza/e_commerce/Controladores/ProductoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -46,3 +47,4 @@ public class ProductoController {
     return ResponseEntity.noContent().build();
   }
 }
+
--- e-commerce/src/main/resources/static/web/assets/JS/index.js
+++ e-commerce/src/main/resources/static/web/assets/JS/index.js
@@ -3,8 +3,48 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            mensaje:`hola`
+            mensaje:`hola`,
+            categoria: [],
+            url:"",
+            productos:"",
+            productosActivos:"",
+            categorias: [],
+            productosFiltrados: [],
+            checked: [],
+            inputBusqueda: "",
         }
-    }
-    }
+    },
+    created(){
+    this.url = "/api/productos";
+    this.loadData() 
+
+    
+    },
+    methods: {
+        loadData: function(){
+            axios.get(this.url)
+            .then( res =>{
+              this.productos= res.data;
+              this.productosActivos= this.productos.filter(producto => producto.Activo == true)
+              this.productosFiltrados = res.data.map(producto => ({ ...producto }));
+              this.categorias = [... new Set(this.productos.map(producto => producto.categoria))];
+              console.log(this.productos)
+            })
+            .catch((error)=>{console.log(error)})
+        },
+    
+        async cargarImagen(event) {
+            const archivo = event.target.files[0];
+            const formData = new FormData();
+            formData.append("imagen", archivo);
+    
+            const respuesta = await axios.post("/productos/imagen", formData);
+            this.producto.imagenUrl = respuesta.data; // Guarda la URL de la imagen
+        },
+        async agregarProducto() {
+            await axios.post("/productos", this.producto);
+            alert("Producto agregado correctamente");
+        }
+    
+    }}
 ).mount('#app')
